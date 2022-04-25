9163702b90f8e6d9897892864b1d2f8371aa8a9d
Justin <25177311+jbudbo@users.noreply.github.com>
2022-04-25 15:23:42 -0400

    poc

---
+++ src/Program.cs
@@ -0,0 +1,17 @@
+﻿string? configPath = Environment.GetEnvironmentVariable("EDGE_CONFIG");
+
+if (string.IsNullOrWhiteSpace(configPath))
+{
+    Console.WriteLine("Config YAML not found");
+    return;
+}
+
+string? content = File.ReadAllText(configPath);
+
+if (string.IsNullOrWhiteSpace(content))
+{
+    Console.WriteLine("Config YAML was empty");
+    return;
+}
+
+Console.WriteLine(content);
